3f3aa7deae6e0ed35eaf17890aca8b3205d1adc6
Avery Fowler <aves.fowler@gmail.com>
2025-02-20 12:10:37 -0700

    submit button changes

--- app.js
+++ app.js
@@ -1,16 +1,15 @@
-document.getElementById("addBtn").addEventListener("click", function() {
+document.getElementById("calcForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+
     let num1 = parseFloat(document.getElementById('add1').value) || 0;
     let num2 = parseFloat(document.getElementById('add2').value) || 0;
     document.getElementById('addResult').innerText = `Result: ${num1 + num2}`;
-});
-document.getElementById("subBtn").addEventListener("click", function() {
-    let num1 = parseFloat(document.getElementById('sub1').value) || 0;
-    let num2 = parseFloat(document.getElementById('sub2').value) || 0;
-    document.getElementById('subResult').innerText = `Result: ${num1 - num2}`;
-});
 
-document.getElementById("mulBtn").addEventListener("click", function() {
-    let num1 = parseFloat(document.getElementById('mul1').value) || 0;
-    let num2 = parseFloat(document.getElementById('mul2').value) || 0;
+    num1 = parseFloat(document.getElementById('sub1').value) || 0;
+    num2 = parseFloat(document.getElementById('sub2').value) || 0;
+    document.getElementById('subResult').innerText = `Result: ${num1 - num2}`;
+
+    num1 = parseFloat(document.getElementById('mul1').value) || 0;
+    num2 = parseFloat(document.getElementById('mul2').value) || 0;
     document.getElementById('mulResult').innerText = `Result: ${num1 * num2}`;
 });
